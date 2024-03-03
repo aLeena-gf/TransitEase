4976e49481c2c17da66c2c299ef8139a2c82d940
Aleena <aleena99george@gmail.com>
2024-03-03 09:49:05 +0530

    Third Commit

--- OneDrive/Desktop/Phoenix/venv/server.py
+++ OneDrive/Desktop/Phoenix/venv/server.py
@@ -1,13 +1,16 @@
-import React from 'react';
 
-const BusSchedule = ({ busScheduleData }) => {
-  return (
-    <div>
-      <h2>Bus Schedule</h2>
-      <p>{busScheduleData.bus_schedule}</p>
-      {/* Render other bus schedule information as needed */}
-    </div>
-  );
-};
+from flask import Flask, jsonify
 
-export default BusSchedule;
+app = Flask(__name__)
+
+sample_bus_schedule = {
+    "bus_schedule": "Bus A: 9:00 AM, 12:00 PM, 3:00 PM\nBus B: 10:00 AM, 1:00 PM, 4:00 PM"
+}
+
+@app.route('/api/bus-schedule', methods=['GET'])
+def get_bus_schedule():
+    return jsonify(sample_bus_schedule)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
